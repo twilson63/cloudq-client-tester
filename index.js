842d2cc7b209e4716ef7ef7b9f364d418d23c18f
Tom <tom@jackhq.com>
2016-01-14 10:18:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http')
+var sendJSON = require('send-data/json')
+var sendError = require('send-data/error')
+var Logger = require('log')
+var log = new Logger('info')
+
+var random = require("random-js")() // uses the nativeMath engine
+
+
+var server = http.createServer((req, res) => {
+  var value = random.integer(1, 6)
+  if (value === 1) {
+    return sendError(req, res, {
+      logger: log,
+      body: {
+        message: 'Gateway Not Available',
+        statusCode: 503
+      },
+      bodyStatusCode: true
+    })
+  }
+  setTimeout(() => sendJSON(req, res, { status: 'empty' }), 10000)
+})
+var port = process.env.PORT || 3000
+server.listen(port)
+console.log('server listening on port 3000')
---
+++ readme.md
@@ -0,0 +1,12 @@
+# cloudq-client-tester
+
+A Test Webserver created to test cloudq-clients so that you can confirm that
+your worker is resilient to failures.
+
+
+## Setup
+
+```
+npm install
+npm start
+```
